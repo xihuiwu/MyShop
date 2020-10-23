7b19d3a5cf81aa41a738f30bb9055bae93424fb4
xihuiwu <51761735+xihuiwu@users.noreply.github.com>
2020-10-23 10:25:46 -0700

    Implemented abstract class and repository for template class

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
@@ -10,11 +10,11 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductCategoryController : Controller
     {
-        ProductCategoryRepository context;
+        InMemoryRepository<ProductCategory> context;
 
         public ProductCategoryController()
         {
-            context = new ProductCategoryRepository();
+            context = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
